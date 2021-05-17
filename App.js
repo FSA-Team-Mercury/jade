e4e37452b27c40f3ff26491417eab204c87eac21
Alan Alban <alan.alban10@gmail.com>
2021-05-17 12:58:10 -0400

    removing warnings

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { AsyncStorage, AppRegistry } from 'react-native';
-console.ignoredYellowBox = ['Warning:'];
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState, useEffect } from "react";
+import { AsyncStorage, AppRegistry, LogBox } from "react-native";
+//LogBox.ignoreLogs(["Warning: ..."]);
+LogBox.ignoreAllLogs();
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import {
   ApolloClient,
   InMemoryCache,
@@ -10,23 +11,23 @@ import {
   createHttpLink,
   ApolloLink,
   gql,
-} from '@apollo/client';
-import Login from './src/screens/login';
-import Home from './src/screens/home';
-import Plaid from './src/screens/Plaid';
-import TabNav from './src/routes/tabNavigator';
-import Signup from './src/screens/signup';
+} from "@apollo/client";
+import Login from "./src/screens/login";
+import Home from "./src/screens/home";
+import Plaid from "./src/screens/Plaid";
+import TabNav from "./src/routes/tabNavigator";
+import Signup from "./src/screens/signup";
 
 const Stack = createStackNavigator();
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3000/graphql',
-  credentials: 'same-origin',
+  uri: "http://localhost:3000/graphql",
+  credentials: "same-origin",
 });
 
 const authLink = new ApolloLink(async (operation, forward) => {
   // Retrieve the authorization token from local storage.
-  const token = await AsyncStorage.getItem('TOKEN');
+  const token = await AsyncStorage.getItem("TOKEN");
   // Use the setContext method to set the HTTP headers.
   operation.setContext({
     headers: {
@@ -38,7 +39,7 @@ const authLink = new ApolloLink(async (operation, forward) => {
 });
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: "http://localhost:3000/graphql",
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
@@ -66,24 +67,24 @@ export default function App() {
     <ApolloProvider client={client}>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName={user ? 'Nav' : 'Login'}
+          initialRouteName={user ? "Nav" : "Login"}
           screenOptions={{
             headerShown: false,
           }}
         >
-          <Stack.Screen name='Login'>
+          <Stack.Screen name="Login">
             {(props) => <Login {...props} setUser={setUser} />}
           </Stack.Screen>
-          <Stack.Screen name='Signup'>
+          <Stack.Screen name="Signup">
             {(props) => <Signup {...props} />}
           </Stack.Screen>
-          <Stack.Screen name='Plaid'>
+          <Stack.Screen name="Plaid">
             {(props) => <Plaid {...props} />}
           </Stack.Screen>
-          <Stack.Screen name='Nav'>
+          <Stack.Screen name="Nav">
             {(props) => <TabNav {...props} client={client} />}
           </Stack.Screen>
-          <Stack.Screen name='Home'>
+          <Stack.Screen name="Home">
             {(props) => <Home {...props} client={client} />}
           </Stack.Screen>
         </Stack.Navigator>
@@ -92,4 +93,4 @@ export default function App() {
   );
 }
 
-AppRegistry.registerComponent('jade', () => App);
+AppRegistry.registerComponent("jade", () => App);
