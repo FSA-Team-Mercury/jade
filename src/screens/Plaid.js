35988373203411e97940d6ee3a30bb78a4ca9e4a
Alan Alban <alan.alban10@gmail.com>
2021-06-01 09:24:45 -0400

    (Plaid) Changed plaid link to expo plaid link. Allows for mobile signup.

--- src/screens/Plaid.js
+++ src/screens/Plaid.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { PLAID_TOKEN_URL } from "@env";
 import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
-import { PlaidLink } from "react-native-plaid-link-sdk";
+// import { PlaidLink } from "react-native-plaid-link-sdk";
 import axios from "axios";
 import { useMutation, useApolloClient } from "@apollo/client";
 import { USER_PLAID_AUTH } from "../queries/user";
 import { FETCH_TOKEN } from "../queries/plaid.js";
+import PlaidLink from "@burstware/expo-plaid-link";
 
 export default function Plaid(props) {
   const [token, setToken] = useState(null);
@@ -36,13 +37,10 @@ export default function Plaid(props) {
       </View>
     );
   }
-
   return (
     <PlaidLink
-      env="sandbox"
-      tokenConfig={{
-        token: token,
-      }}
+      linkToken={token}
+      onEvent={(event) => console.log(event)}
       onSuccess={async (success) => {
         try {
           await getPlaid({
@@ -61,15 +59,50 @@ export default function Plaid(props) {
       onExit={(exit) => {
         console.log(exit);
       }}
-    >
-      <View style={styles.container}>
-        <Text>Let&apos;s Add your bank account information!</Text>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Add Account</Text>
-        </View>
-      </View>
-    </PlaidLink>
+    />
   );
+
+  // *** OLD PLAID INTEGRATION *** //
+  // * This integration works, but it will not allow you to sign up on your phone. Works on xcode simulator only.
+  // * This is better suited for a non-Expo React Native App
+  // * To use it, comment out the return statement above and uncomment the one below this
+  // * The expo-plaid-link version uses webview, so there is no button to take you to the
+  //   plaid portal, it goes there automatically
+  // * Functionality once you signup is the same for both.
+
+  // return (
+  //   <PlaidLink
+  //     env="sandbox"
+  //     tokenConfig={{
+  //       token: token,
+  //     }}
+  //     onSuccess={async (success) => {
+  //       try {
+  //         await getPlaid({
+  //           variables: {
+  //             public_token: success.publicToken,
+  //           },
+  //         });
+  //         props.navigation.reset({
+  //           index: 0,
+  //           routes: [{ name: "Nav" }],
+  //         });
+  //       } catch (err) {
+  //         throw new Error("Error Connectinig to Plaid", err);
+  //       }
+  //     }}
+  //     onExit={(exit) => {
+  //       console.log(exit);
+  //     }}
+  //   >
+  //     <View style={styles.container}>
+  //       <Text>Let&apos;s Add your bank account information!</Text>
+  //       <View style={styles.button}>
+  //         <Text style={styles.buttonText}>Add Account</Text>
+  //       </View>
+  //     </View>
+  //   </PlaidLink>
+  // );
 }
 
 const styles = StyleSheet.create({
